7d79e8965c20d1b8e313e037e90b5f4f795126a0
Layla <layla.hisham.sami@gmail.com>
2025-07-24 09:53:39 -0700

    Initial Commit

---
+++ nav/navApp.swift
@@ -0,0 +1,17 @@
+//
+//  navApp.swift
+//  nav
+//
+//  Created by Layla Hisham on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct navApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
